3cbf99b344388ca5566aa7393cc462014abba38b
dev-marian-vybyranyi <marian.vybyranyi.dev@gmail.com>
2026-01-19 16:04:15 +0200

    Remove App.css, integrate Ant Design and Bootstrap for UI, and add React Router DOM.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import './App.css'
+import { Button } from 'antd'
 
 function App() {
 
   return (
-    <>
-    </>
+    <div className='m-5'>
+    <h1>Job Portal</h1>
+    <Button type="primary">Primary Button</Button>
+    </div>
   )
 }
 
