a5f6b5352a83c35504ec6bdcf7814986806aec51
dev-marian-vybyranyi <marian.vybyranyi.dev@gmail.com>
2026-01-19 16:39:18 +0200

    Implement registration form UI with Ant Design components and basic form handling.

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import { Form } from "antd";
+import { Link } from "react-router-dom";
 
 const Register = () => {
-  return (
-    <div>Register</div>
-  )
-}
+  const onFinish = (values) => {
+    console.log(values);
+  };
 
-export default Register
+  return (
+    <div className="h-screen d-flex align-items-center  justify-content-center bg-primary">
+      <div className="bg-white p-4 w-400">
+        <h4>Job Portal - Register</h4>
+        <div className="divider"></div>
+        <Form layout="vertical ">
+          <Form.Item name="name" label="Name">
+            <input type="text" />
+          </Form.Item>
+          <Form.Item name="email" label="Email">
+            <input type="text" />
+          </Form.Item>{" "}
+          <Form.Item name="password" label="Password">
+            <input type="password" />
+          </Form.Item>
+          <button className="primary-contained-btn w-100 mt-2">Register</button>
+          <Link to="/login" className="d-block mt-2">
+            Already have an account?
+          </Link>
+        </Form>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
