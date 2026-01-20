13a7f284857857b39735f49e80edd8cc28b45326
dev-marian-vybyranyi <marian.vybyranyi.dev@gmail.com>
2026-01-20 13:27:12 +0200

    Fetch and display approved job listings on the Home page with detailed cards and navigation to job descriptions.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,81 @@
-const Home = () => {
+import { Col, message, Row } from "antd";
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { getAllJobs } from "../apis/jobs";
+import PageTitle from "../components/PageTitle";
+import { HideLoading, ShowLoading } from "../redux/alertSlice";
+
+function Home() {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const [data, setData] = useState([]);
+
+  const getData = async () => {
+    try {
+      dispatch(ShowLoading());
+      const response = await getAllJobs();
+      if (response.success) {
+        const approvedJobs = response.data.filter(
+          (job) => job.status === "approved",
+        );
+        setData(approvedJobs);
+      }
+      dispatch(HideLoading());
+    } catch (error) {
+      dispatch(HideLoading());
+      message.error(error.message);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
   return (
-    <div>Home</div>
-  )
+    <div>
+      <PageTitle title="Home" />
+      <Row gutter={[15, 15]} className="mt-3">
+        {data.map((job) => (
+          <Col span={8}>
+            <div className="job-card">
+              <h3 className="uppercase">{job.title}</h3>
+              <div className="light-divider"></div>
+
+              <div className="d-flex flex-column gap-1">
+                <div className="d-flex justify-content-between mt-1">
+                  <span>Company</span>
+                  <span>{job.company}</span>
+                </div>
+                <div className="d-flex justify-content-between">
+                  <span>Location</span>
+                  <span>{job.location}</span>
+                </div>
+                <div className="d-flex justify-content-between">
+                  <span>Salary</span>
+                  <span>{job.salary}</span>
+                </div>
+                <div className="d-flex justify-content-between">
+                  <span>Posted On</span>
+                  <span>{job.postedOn}</span>
+                </div>
+                <div className="d-flex justify-content-between">
+                  <span>Last Date To Apply</span>
+                  <span>{job.lastDateToApply}</span>
+                </div>
+              </div>
+
+              <button
+                className="primary-outlined-btn w-100 mt-2"
+                onClick={() => navigate(`/job-description/${job.id}`)}
+              >
+                Apply Now
+              </button>
+            </div>
+          </Col>
+        ))}
+      </Row>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
